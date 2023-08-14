a9ef24dbaeff5f12717b17b46c279ec72104c5f7
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-15 00:11:16 +0530

    [Coding]: Added Intents and Various improvements. Added Intents to move to Login page if user Has Already registered in the Application.
    Added OnClickListener to Register Button to register and login Simultaneously into the application.
    
    Also Added way to Directly Open HomePage if User is Already LoggedIn.
    
    Also Added Check For password and retyped password are matching or not.
    
    Also Set Registration page as main Startup Page.

--- app/src/main/java/com/nizam/music_player/RegistrationActivity.kt
+++ app/src/main/java/com/nizam/music_player/RegistrationActivity.kt
@@ -1,11 +1,72 @@
 package com.nizam.music_player
 
+import DBLogin
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var uname: EditText
+    private lateinit var uemail: EditText
+    private lateinit var pwd: EditText
+    private lateinit var retypedPwd: EditText
+    private lateinit var register: Button
+    private lateinit var alreadyRegistered: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        val userManager:UserManager = UserManager(applicationContext)
+        if(userManager.isUserLoggedIn()) {
+            intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            println("GoodBye")
+        }
+        uname = findViewById(R.id.userName_edtxt)
+        uemail = findViewById(R.id.email_edtxt)
+        pwd = findViewById(R.id.pwd_edtxt)
+        retypedPwd = findViewById(R.id.retypepwd_edtxt)
+
+        register = findViewById(R.id.register_btn)
+        alreadyRegistered = findViewById(R.id.alreadyRegistered_btn)
+
+        alreadyRegistered.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                startActivity(Intent(applicationContext,MainActivity::class.java))
+            }
+        })
+
+        register.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val db = DBLogin(applicationContext,null)
+
+                val name = uname.text.toString()
+                val email = uemail.text.toString()
+                val passwd = pwd.text.toString()
+                val rePasswd = retypedPwd.text.toString()
+                if( passwd == rePasswd) {
+                    try {
+                        db.registerUser(name, email, passwd)
+                    }
+                    catch (e :Exception) {
+
+                    }
+                    userManager.setUserLoggedIn(true)
+                    Toast.makeText(applicationContext,"User Registered Successfully",Toast.LENGTH_SHORT).show()
+                    intent = Intent(applicationContext,MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(applicationContext,"Passwords are not Matching Enter Correctly",Toast.LENGTH_SHORT).show()
+                    retypedPwd.error = "Passwords Not Matching"
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/nizam/music_player/UserManager.kt
@@ -0,0 +1,19 @@
+package com.nizam.music_player
+
+import android.content.Context
+
+class UserManager(private val context: Context) {
+
+    private val sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+    private val loggedInKey = "logged_in"
+
+    fun setUserLoggedIn(loggedIn: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(loggedInKey, loggedIn)
+        editor.apply()
+    }
+
+    fun isUserLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean(loggedInKey, false)
+    }
+}
