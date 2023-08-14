c36bfa4545ee32011dadf655be7046c4ce249de4
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-15 00:08:11 +0530

    [Coding]: Added Intents to move to Registration page if user Has no yet registered in the Application.

--- app/src/main/java/com/nizam/music_player/LoginActivity.kt
+++ app/src/main/java/com/nizam/music_player/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.nizam.music_player
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var registerbtn: Button = findViewById(R.id.register_btn)
+        registerbtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                intent = Intent(applicationContext, RegistrationActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
