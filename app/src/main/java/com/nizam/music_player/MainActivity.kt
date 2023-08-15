3e161baa8bb14b80c38b147f8887a63912e18c17
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-15 17:51:13 +0530

    [Coding] : Added Code For OnClick on Login Button.
    ----------------------------------------------------
    Added OnClickListener which checks if user is registered or not and if user is registered it checks the password and if the password matches with one in database user is allowed to pass to HomePage.
    
    Added Button on MainActivity for Debugging Purpose.

--- app/src/main/java/com/nizam/music_player/MainActivity.kt
+++ app/src/main/java/com/nizam/music_player/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.nizam.music_player
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userManager:UserManager = UserManager(applicationContext)
+
+        val btn = findViewById(R.id.testbtn) as Button
+        btn.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                startActivity(Intent(applicationContext,RegistrationActivity::class.java))
+                userManager.setUserLoggedIn(false)
+            }
+        })
     }
 }
