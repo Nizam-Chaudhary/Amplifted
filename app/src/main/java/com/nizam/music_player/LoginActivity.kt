5b3105b0bb991737d5d8f65d75ab05a3c68ffb21
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-15 17:51:13 +0530

    [Coding] : Added Code For OnClick on Login Button.
    ----------------------------------------------------
    Added OnClickListener which checks if user is registered or not and if user is registered it checks the password and if the password matches with one in database user is allowed to pass to HomePage.
    
    Added Button on MainActivity for Debugging Purpose.

--- app/src/main/java/com/nizam/music_player/LoginActivity.kt
+++ app/src/main/java/com/nizam/music_player/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.nizam.music_player
 
+import DBLogin
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -13,13 +15,41 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //OnClickListener for Register Button to open Registration Page to Register The user if not Registered Already.
-        var registerbtn: Button = findViewById(R.id.register_btn)
-        registerbtn.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                intent = Intent(applicationContext, RegistrationActivity::class.java)
-                startActivity(intent)
+        //Defined and find View Id.
+        val uname : EditText = findViewById(R.id.userName_edtxt)
+        val pwd : EditText = findViewById(R.id.pwd_edtxt)
+        val login : Button = findViewById(R.id.login_btn)
+
+        //onClickListener for Login Button to Login To Application If all validations are passed.
+        login.setOnClickListener {
+
+
+            val db = DBLogin(this@LoginActivity,null)
+
+            val name = uname.text.toString()
+            val passwd = pwd.text.toString()
+
+            //Checking if User is Available or not if not give error.
+            if(!db.isAvailable(name)) {
+                uname.error = "No User is available with this Name"
+            } else {
+                //Checking password is correct or not.
+                if(db.validateUser(name,passwd)) {
+                    Toast.makeText(this@LoginActivity,"Logged In Successfully",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                } else {
+                    //error on incorrect password.
+                    pwd.error = "Incorrect Password"
+                }
             }
-        })
+        }
+
+        //OnClickListener for Register Button to open Registration Page to Register The user if not Registered Already.
+        val registerbtn: Button = findViewById(R.id.register_btn)
+
+        registerbtn.setOnClickListener {
+            intent = Intent(applicationContext, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
