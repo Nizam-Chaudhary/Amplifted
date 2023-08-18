26c4c97e889750855d2f900c74fbf6cb102eab7a
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-19 00:21:22 +0530

    Created Design for Favorite and Playlists Layout.
    Added CardView which contain back and Name of the screen in both the layout.
    
    Added the floating action button for shuffling song from liked songs on Favorite Page.
    
    Added the floating action button for creating new Playlists.

---
+++ app/src/main/java/com/nizam/music_player/FavoriteActivity.kt
@@ -0,0 +1,11 @@
+package com.nizam.music_player
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
