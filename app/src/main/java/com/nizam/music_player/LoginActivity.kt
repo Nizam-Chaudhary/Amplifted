0781004a30987cf74fcc6e06731a72f9a36d4cf7
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-19 18:58:44 +0530

    Added ViewBinding and removed findViewbyid

--- app/src/main/java/com/nizam/music_player/LoginActivity.kt
+++ app/src/main/java/com/nizam/music_player/LoginActivity.kt
@@ -2,35 +2,31 @@ package com.nizam.music_player
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.nizam.music_player.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Defined and find View Id.
-        val uname: EditText = findViewById(R.id.userName_edtxt)
-        val pwd: EditText = findViewById(R.id.pwd_edtxt)
-        val login: Button = findViewById(R.id.login_btn)
 
         //onClickListener for Login Button to Login To Application If all validations are passed.
-        login.setOnClickListener {
+        binding.loginBtn.setOnClickListener {
 
 
             val db = DBLogin(this@LoginActivity, null)
 
-            val name = uname.text.toString()
-            val passwd = pwd.text.toString()
+            val name = binding.userNameEdtxt.text.toString()
+            val passwd = binding.pwdEdtxt.text.toString()
 
             //Checking if User is Available or not if not give error.
             if (!db.isAvailable(name)) {
-                uname.error = "No User is available with this Name"
+                binding.userNameEdtxt.error = "No User is available with this Name"
             } else {
                 //Checking password is correct or not.
                 if (db.validateUser(name, passwd)) {
@@ -45,17 +41,14 @@ class LoginActivity : AppCompatActivity() {
                     startActivity(Intent(this@LoginActivity, MainActivity::class.java))
                 } else {
                     //error on incorrect password.
-                    pwd.error = "Incorrect Password"
+                    binding.pwdEdtxt.error = "Incorrect Password"
                 }
             }
         }
 
         //OnClickListener for Register Button to open Registration Page to Register The user if not Registered Already.
-        val registerBtn: Button = findViewById(R.id.register_btn)
-
-        registerBtn.setOnClickListener {
-            intent = Intent(applicationContext, RegistrationActivity::class.java)
-            startActivity(intent)
+        binding.registerBtn.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
         }
     }
 }
