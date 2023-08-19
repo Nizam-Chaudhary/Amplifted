4c2844410e0dc8883a4959e1856b66c7ce51f725
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-19 18:37:53 +0530

    Added Id's to the widget and Added Intent.

--- app/src/main/java/com/nizam/music_player/MainActivity.kt
+++ app/src/main/java/com/nizam/music_player/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.nizam.music_player
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.nizam.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.shuffleButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity,PlaylistActivity::class.java))
+        }
+
+        binding.favoritesButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity,FavoriteActivity::class.java))
+        }
+
+        binding.playlistsButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity,PlaylistActivity::class.java))
+        }
     }
 }
