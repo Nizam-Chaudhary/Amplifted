4740a5b47b0f1eceb7c45c3659c214f175a22b0b
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-20 14:41:12 +0530

    Changed Design of Favorites layout.

--- app/src/main/java/com/nizam/music_player/FavoriteActivity.kt
+++ app/src/main/java/com/nizam/music_player/FavoriteActivity.kt
@@ -7,5 +7,13 @@ class FavoriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+
+        supportActionBar?.setTitle(R.string.favorites)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
