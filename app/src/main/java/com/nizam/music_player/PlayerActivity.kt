f39ed1aae63df7ad8f88fd6117f9dffafe904e71
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-20 14:37:01 +0530

    Changed parent theme to Theme.Material3.DayNight for the app.

--- app/src/main/java/com/nizam/music_player/PlayerActivity.kt
+++ app/src/main/java/com/nizam/music_player/PlayerActivity.kt
@@ -1,11 +1,17 @@
 package com.nizam.music_player
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
