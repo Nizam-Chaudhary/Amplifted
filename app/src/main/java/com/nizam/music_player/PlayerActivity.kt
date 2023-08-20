c76c5490e49806baea78d1a63e98434b49a7a38b
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-20 22:55:09 +0530

    Added PlayPause and Added image and name of song.

--- app/src/main/java/com/nizam/music_player/PlayerActivity.kt
+++ app/src/main/java/com/nizam/music_player/PlayerActivity.kt
@@ -3,32 +3,41 @@ package com.nizam.music_player
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.nizam.music_player.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityPlayerBinding
+    private var isSongPlaying = false
     companion object {
         var musicListPA = ArrayList<SongsData>()
         var songPosition = 0
         var mediaPlayer: MediaPlayer? = null
     }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         songPosition = intent.getIntExtra("index",0)
-        when(intent.getStringExtra("class")) {
-            "MusicAdapter" -> {
-                musicListPA.addAll(MainActivity.musicListMA)
-                if(mediaPlayer == null) {
-                    mediaPlayer = MediaPlayer()
-                } else {
-                    mediaPlayer!!.reset()
-                    mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
-                    mediaPlayer!!.prepare()
-                    mediaPlayer!!.start()
-                }
 
+        when(intent.getStringExtra("class")) {
+            "MusicAdapter" -> createMediaPlayer()
+        }
+
+        playPauseSong()
+    }
+
+    private fun playPauseSong() {
+        //Pause Or Play Button OnClickListener
+        binding.pausePlayButton.setOnClickListener{
+            if(isSongPlaying) {
+                pauseMusic()
+            } else {
+                playMusic()
             }
         }
     }
@@ -36,5 +45,40 @@ class PlayerActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    private fun createMediaPlayer() {
+        try{
+            musicListPA.addAll(MainActivity.musicListMA)
+            setLayout()
+            if(mediaPlayer == null) {
+                mediaPlayer = MediaPlayer()
+            } else {
+                mediaPlayer!!.reset()
+                mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+                mediaPlayer!!.prepare()
+                mediaPlayer!!.start()
+                isSongPlaying = true
+                binding.pausePlayButton.setIconResource(R.drawable.pause_icon)
+            }
+        } catch(e:Exception) {}
+    }
+    private fun setLayout() {
+        Glide.with(this@PlayerActivity)
+            .load(musicListPA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.icon).centerCrop())
+            .into(binding.albumImage)
+        binding.songName.text = musicListPA[songPosition].title
+    }
+
+    private fun playMusic() {
+        binding.pausePlayButton.setIconResource(R.drawable.pause_icon)
+        isSongPlaying = true
+        mediaPlayer!!.start()
+    }
+    private fun pauseMusic() {
+        binding.pausePlayButton.setIconResource(R.drawable.play_icon)
+        isSongPlaying = false
+        mediaPlayer!!.pause()
     }
 }
