3a05d27b0396bef7047bf81126533a19cfafdcb2
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-20 12:20:13 +0530

    Created Function to request Permission when the application is started.
    also added permission required by the application in the AndroidManifest.xml file.

--- app/src/main/java/com/nizam/music_player/MainActivity.kt
+++ app/src/main/java/com/nizam/music_player/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.nizam.music_player
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nizam.music_player.databinding.ActivityMainBinding
 
@@ -22,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
         setContentView(binding.root)
+        updateOrRequestPermission()
 
         //Checking for user logged in Status and If user Is logged Then he does not need to enter details and directly jump back to home Screen.
         binding.shuffleButton.setOnClickListener {
@@ -34,6 +40,54 @@ class MainActivity : AppCompatActivity() {
 
         binding.playlistsButton.setOnClickListener {
             startActivity(Intent(this@MainActivity,PlaylistActivity::class.java))
+        }
+    }
+
+    //For Requesting Runtime Permission.
+    private fun updateOrRequestPermission() {
+        if(ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.READ_MEDIA_AUDIO) == PackageManager.PERMISSION_DENIED ){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_MEDIA_AUDIO),1)
+            }
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            if(ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ){
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),2)
+            }
+        }
+
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+
+            if(requestCode == 1) {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this@MainActivity,"Permission Granted",Toast.LENGTH_SHORT).show()
+                } else {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_MEDIA_AUDIO),1)
+                    Toast.makeText(this@MainActivity,"Permission Not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+
+            if(requestCode == 2) {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this@MainActivity,"Permission Granted",Toast.LENGTH_SHORT).show()
+                } else {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),2)
+                    Toast.makeText(this@MainActivity,"Permission Not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
