bc977e521f7dec79c614b6657421f2e6883849af
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-21 23:50:41 +0530

    created ApplicationClass to be used to create notification.
    This will be used to display notifications.

---
+++ app/src/main/java/com/nizam/music_player/ApplicationClass.kt
@@ -0,0 +1,26 @@
+package com.nizam.music_player
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class ApplicationClass: Application() {
+    companion object{
+        const val CHANNEL_ID= "Channel 1"
+        const val PLAY_PAUSE= "PlayPause"
+        const val NEXT= "Next"
+        const val PREVIOUS= "Previous"
+        const val EXIT= "Exit"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(CHANNEL_ID,"Now Playing", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.description = "It is important channel to play songs."
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
