9e58827659d203784730f89f2ccec2d803dc96f8
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-22 09:16:12 +0530

    Refactored the code.

--- app/src/main/java/com/nizam/music_player/MusicService.kt
+++ app/src/main/java/com/nizam/music_player/MusicService.kt
@@ -9,36 +9,40 @@ import android.os.IBinder
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.core.app.NotificationCompat
 
-class MusicService:Service() {
+class MusicService : Service() {
     private var myBinder = MyBinder()
-    var mediaPlayer:MediaPlayer? = null
+    var mediaPlayer: MediaPlayer? = null
     private lateinit var mediaSession: MediaSessionCompat
 
     override fun onBind(intent: Intent?): IBinder {
-        mediaSession = MediaSessionCompat(baseContext,"Amplifted")
+        mediaSession = MediaSessionCompat(baseContext, "Amplifted")
         return myBinder
     }
-    inner class MyBinder: Binder(){
-        fun currentService():MusicService{
+
+    inner class MyBinder : Binder() {
+        fun currentService(): MusicService {
             return this@MusicService
         }
     }
 
     fun showNotification() {
-        val notification = NotificationCompat.Builder(baseContext,ApplicationClass.CHANNEL_ID)
+        val notification = NotificationCompat.Builder(baseContext, ApplicationClass.CHANNEL_ID)
             .setContentTitle(PlayerActivity.musicListPA[PlayerActivity.songPosition].title)
             .setContentText(PlayerActivity.musicListPA[PlayerActivity.songPosition].artist)
             .setSmallIcon(R.drawable.playlist_icon)
-            .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.icon))
-            .setStyle(androidx.media.app.NotificationCompat.MediaStyle().setMediaSession(mediaSession.sessionToken))
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.icon))
+            .setStyle(
+                androidx.media.app.NotificationCompat.MediaStyle()
+                    .setMediaSession(mediaSession.sessionToken)
+            )
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setOnlyAlertOnce(true)
-            .addAction(R.drawable.previous_icon,"Previous",null)
-            .addAction(R.drawable.play_icon,"Play",null)
-            .addAction(R.drawable.next_icon,"Next",null)
-            .addAction(R.drawable.exit_icon,"Exit",null)
+            .addAction(R.drawable.previous_icon, "Previous", null)
+            .addAction(R.drawable.play_icon, "Play", null)
+            .addAction(R.drawable.next_icon, "Next", null)
+            .addAction(R.drawable.exit_icon, "Exit", null)
             .build()
-        startForeground(7,notification)
+        startForeground(7, notification)
     }
 }
