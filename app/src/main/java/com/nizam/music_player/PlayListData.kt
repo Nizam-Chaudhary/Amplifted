9da947d97ece5a0a1ea1bc7517dc94ca16ee7266
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-26 20:06:23 +0530

    implemented adapter for playList Recycler view.

--- app/src/main/java/com/nizam/music_player/PlayListData.kt
+++ app/src/main/java/com/nizam/music_player/PlayListData.kt
@@ -1,3 +1,3 @@
 package com.nizam.music_player
 
-data class PlayListData(val playListName:String,val playListImageArt:String)
+data class PlayListData(val playListName:String,val playListImageArt:String?)
--- app/src/main/java/com/nizam/music_player/PlaylistActivity.kt
+++ app/src/main/java/com/nizam/music_player/PlaylistActivity.kt
@@ -1,12 +1,25 @@
 package com.nizam.music_player
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.nizam.music_player.databinding.ActivityPlaylistBinding
 
 class PlaylistActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlaylistBinding
+
+    private val playListDB:PlaylistsDB by lazy {
+       PlaylistsDB(this@PlaylistActivity,null)
+   }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_playlist)
+
+        binding = ActivityPlaylistBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         //setting title and back button on ToolBar.
         supportActionBar?.setTitle(R.string.playlists)
@@ -16,5 +29,26 @@ class PlaylistActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    private fun setRecyclerViewAdapter() {
+        Log.i("Favorites","recyclerView")
+        binding.playListRecyclerView.setHasFixedSize(true)
+        binding.playListRecyclerView.setItemViewCacheSize(10)
+        binding.playListRecyclerView.layoutManager = GridLayoutManager(this@PlaylistActivity,2)
+
+        val playListRecyclerViewAdapter = PlayListRecyclerViewAdapter(this@PlaylistActivity,getAllPlayList())
+        binding.playListRecyclerView.adapter = playListRecyclerViewAdapter
+    }
+
+    private fun getAllPlayList():ArrayList<PlayListData> {
+        val list:ArrayList<PlayListData> = ArrayList()
+        val tempList = playListDB.getPlayListNames()
+
+        for(i in tempList) {
+            list.add(PlayListData(i,playListDB.getPlayListArtUri(i)))
+        }
+
+        return list
     }
 }
