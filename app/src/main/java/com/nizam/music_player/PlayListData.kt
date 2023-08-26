9bc7157c11336f0fa4d9f6be18d9b7f1cc9bc67a
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-26 19:04:05 +0530

    Created adapter for PlayLists Page which Lists all the playlist.

---
+++ app/src/main/java/com/nizam/music_player/PlayListData.kt
@@ -0,0 +1,3 @@
+package com.nizam.music_player
+
+data class PlayListData(val playListName:String,val playListImageArt:String)
---
+++ app/src/main/java/com/nizam/music_player/PlayListRecyclerViewAdapter.kt
@@ -0,0 +1,53 @@
+package com.nizam.music_player
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.nizam.music_player.databinding.FavoritesRecyclerViewBinding
+
+class PlayListRecyclerViewAdapter(private val context: Context,private val playList: ArrayList<PlayListData>):RecyclerView.Adapter<PlayListRecyclerViewAdapter.Holder>(){
+
+    /*private val playListDB:PlaylistsDB by lazy {
+        PlaylistsDB(context,null)
+    }*/
+
+    class Holder(binding: FavoritesRecyclerViewBinding):RecyclerView.ViewHolder(binding.root) {
+        val playListName = binding.favoritesSongName
+        val playListImage = binding.favoritesAlbumArt
+        val root = binding.root
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): Holder {
+        return Holder(FavoritesRecyclerViewBinding.inflate(LayoutInflater.from(context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.playListName.text = playList[position].playListName
+        //setting image of album.
+        Glide.with(context)
+            .load(playList[position].playListImageArt)
+            .apply(RequestOptions().placeholder(R.drawable.icon).centerCrop())
+            .into(holder.playListImage)
+
+        //Open Playlist
+        openPlayList(holder, position)
+    }
+
+    private fun openPlayList(holder: Holder, position: Int) {
+        holder.root.setOnClickListener {
+            Toast.makeText(context,playList[position].playListName,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return playList.size
+    }
+
+}
