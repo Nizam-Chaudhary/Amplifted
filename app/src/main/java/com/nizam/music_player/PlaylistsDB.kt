093103907cc958e4c6d6ea8f06e18c90efe189e4
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-26 18:53:23 +0530

    Added PlayListMaster Table which stores list of all Playlist.
    Also added function to get list of playlist.
    
    added function to createPlaylist.

--- app/src/main/java/com/nizam/music_player/PlaylistsDB.kt
+++ app/src/main/java/com/nizam/music_player/PlaylistsDB.kt
@@ -6,54 +6,78 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class PlaylistsDB(context: Context, factory: SQLiteDatabase.CursorFactory?, private val playlistName: String):
+class PlaylistsDB(context: Context, factory: SQLiteDatabase.CursorFactory?):
     SQLiteOpenHelper(context, DATABASE_NAME,factory, DATABASE_VERSION) {
 
         companion object {
             const val DATABASE_NAME = "Playlists"
             const val DATABASE_VERSION = 1
+            const val PLAYLIST_MASTER = "PlayListMaster"
+            const val PLAYLIST = "PlayList"
+
+            const val PLAYLIST_NAME_COL = "playListName"
+            const val ID_COL = "id"
+            const val TITLE_COL = "title"
+            const val ALBUM_COL = "album"
+            const val ARTIST_COL = "artist"
+            const val DURATION_COL = "duration"
+            const val PATH_COL = "path"
+            const val ART_URI_COL = "artUri"
         }
 
 
     //this function creates the table for playlist if it does not exists.
     override fun onCreate(p0: SQLiteDatabase?) {
-        val query = "CREATE TABLE IF NOT EXISTS $playlistName(${FavoritesDB.ID_COL} TEXT PRIMARY KEY, ${FavoritesDB.TITLE_COL} TEXT, ${FavoritesDB.ALBUM_COL} TEXT, ${FavoritesDB.ARTIST_COL} TEXT, ${FavoritesDB.DURATION_COL} INTEGER, ${FavoritesDB.PATH_COL} TEXT, ${FavoritesDB.ART_URI_COL} TEXT)"
+        var query = "Create Table If Not Exists $PLAYLIST_MASTER($PLAYLIST_NAME_COL TEXT Primary Key)"
+        p0?.execSQL(query)
+        query = "CREATE TABLE IF NOT EXISTS $PLAYLIST($PLAYLIST_NAME_COL TEXT,$ID_COL TEXT PRIMARY KEY, $TITLE_COL TEXT, $ALBUM_COL TEXT, $ARTIST_COL TEXT, $DURATION_COL INTEGER, $PATH_COL TEXT, $ART_URI_COL TEXT)"
         p0?.execSQL(query)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        p0?.execSQL("DROP TABLE IF EXISTS $playlistName")
+        p0?.execSQL("DROP TABLE IF EXISTS $PLAYLIST_MASTER")
+        p0?.execSQL("DROP TABLE IF EXISTS $PLAYLIST")
         onCreate(p0)
     }
 
+    fun createPlayList(playListName:String) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(PLAYLIST_NAME_COL,playListName)
+        db.insert(PLAYLIST_MASTER,null,values)
+    }
+
     //this function is used to add the song to the playlist
-    fun addToPlaylist(songsData: SongsData) {
+    fun addToPlaylist(songsData: SongsData,playListName:String) {
 
         val values = ContentValues()
 
-        values.put(FavoritesDB.ID_COL,songsData.id)
-        values.put(FavoritesDB.TITLE_COL,songsData.title)
-        values.put(FavoritesDB.ALBUM_COL,songsData.album)
-        values.put(FavoritesDB.ARTIST_COL,songsData.artist)
-        values.put(FavoritesDB.DURATION_COL,songsData.duration)
-        values.put(FavoritesDB.PATH_COL,songsData.path)
-        values.put(FavoritesDB.ART_URI_COL,songsData.artUri)
+        values.put(PLAYLIST_NAME_COL,playListName)
+        values.put(ID_COL,songsData.id)
+        values.put(TITLE_COL,songsData.title)
+        values.put(ALBUM_COL,songsData.album)
+        values.put(ARTIST_COL,songsData.artist)
+        values.put(DURATION_COL,songsData.duration)
+        values.put(PATH_COL,songsData.path)
+        values.put(ART_URI_COL,songsData.artUri)
 
         val db = this.writableDatabase
-        db.insert(playlistName,null,values)
+        db.insert(PLAYLIST,null,values)
 
         db.close()
     }
 
-    fun removeFromFavorites(name:String) {
+    fun removeFromPlayList(playListName: String,songName:String) {
         val db = this.writableDatabase
-        val query = "DELETE FROM $playlistName WHERE ${FavoritesDB.TITLE_COL}= '$name'"
+        val query = "DELETE FROM $PLAYLIST WHERE $TITLE_COL= '$songName' And $PLAYLIST_NAME_COL = '$playListName'"
         db.execSQL(query)
     }
 
     fun removePlaylist(playlistName: String) {
         val db=this.writableDatabase
-        val query = "DROP TABLE $playlistName"
+        var query = "Delete From $PLAYLIST_MASTER where $PLAYLIST_NAME_COL = '$playlistName' "
+        db.execSQL(query)
+        query = "Delete From $PLAYLIST where $PLAYLIST_NAME_COL = '$playlistName'"
         db.execSQL(query)
     }
 
@@ -61,7 +85,7 @@ class PlaylistsDB(context: Context, factory: SQLiteDatabase.CursorFactory?, priv
     fun songExists(name: String): Boolean {
         val db = this.readableDatabase
 
-        val query = "SELECT ${FavoritesDB.ID_COL} FROM $playlistName WHERE ${FavoritesDB.TITLE_COL} = '$name'"
+        val query = "SELECT $ID_COL FROM $PLAYLIST WHERE $TITLE_COL = '$name'"
         val cursor = db.rawQuery(query,null)
         var value: String? = null
         if(cursor.moveToFirst())
@@ -70,5 +94,19 @@ class PlaylistsDB(context: Context, factory: SQLiteDatabase.CursorFactory?, priv
         if(value != null)
             return true
         return false
+    }
+
+    @SuppressLint("Range")
+    fun getPlayListNames(): ArrayList<String> {
+        val list:ArrayList<String> = ArrayList()
+        val db = this.readableDatabase
+        val query = "SELECT $PLAYLIST_NAME_COL FROM $PLAYLIST_MASTER"
+        val cursor = db.rawQuery(query,null)
+        if(cursor.moveToFirst()) {
+            do {
+                list.add(cursor.getString(cursor.getColumnIndex(PLAYLIST_NAME_COL)))
+            }while (cursor.moveToNext())
+        }
+        return list
     }
 }
