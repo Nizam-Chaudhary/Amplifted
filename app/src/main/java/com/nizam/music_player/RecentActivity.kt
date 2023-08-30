2df058c9f21ecfd495a0a4329ee3a7a1911cfb6f
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-08-30 13:32:08 +0530

    added function setAdapter.
    this function is used to set adapter to recent recycler view adapter and fetch all songs from the database too.

--- app/src/main/java/com/nizam/music_player/RecentActivity.kt
+++ app/src/main/java/com/nizam/music_player/RecentActivity.kt
@@ -2,10 +2,18 @@ package com.nizam.music_player
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nizam.music_player.databinding.ActivityRecentBinding
 
 class RecentActivity : AppCompatActivity() {
     lateinit var binding: ActivityRecentBinding
+    private val recentDB: RecentDB by lazy {
+        RecentDB(this@RecentActivity,null)
+    }
+    companion object {
+        var musicListRP: ArrayList<SongsData> = ArrayList()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,7 +23,16 @@ class RecentActivity : AppCompatActivity() {
         supportActionBar?.title = "Recently Played"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        setAdapter()
+    }
 
+    private fun setAdapter() {
+        binding.recentRecyclerView.setHasFixedSize(true)
+        binding.recentRecyclerView.setItemViewCacheSize(13)
+        binding.recentRecyclerView.layoutManager = LinearLayoutManager(this@RecentActivity)
+        musicListRP = recentDB.getAllSongs()
+        val adapter = RecentRecyclerViewAdapter(this@RecentActivity, musicListRP)
+        binding.recentRecyclerView.adapter = adapter
     }
 
     override fun onSupportNavigateUp(): Boolean {
